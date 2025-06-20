485d0c6f22fffe317ea0601ce6d5c3c7d7af77d2
mezemime <imene.mezemate@univ-rouen.fr>
2025-06-20 17:44:58 +0200

    demo.py

--- demo/demo.py
+++ demo/demo.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from face_detection import face_detection
 
 
-face_list, fps = face_detection(video_path='./my_video.avi')
+face_list, fps = face_detection(video_path="C:/Users/Dell/Desktop/SD/stage/videos/seance11/vid76.mp4")
 
 print('\nrPPG estimation')
 device = torch.device('cpu')
@@ -15,7 +15,7 @@ device = torch.device('cpu')
 with torch.no_grad():
     face_list = torch.tensor(face_list.astype('float32')).to(device)
     model = PhysNet(S=2).to(device).eval()
-    model.load_state_dict(torch.load('./model_weights.pt', map_location=device))
+    model.load_state_dict(torch.load('./demo/model_weights.pt', map_location=device))
     rppg = model(face_list)[:,-1, :]
     rppg = rppg[0].detach().cpu().numpy()
     rppg = butter_bandpass(rppg, lowcut=0.6, highcut=4, fs=fps)
